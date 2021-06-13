145f84b1ab6cf33d2bb8e08eb8ef57001b3d97c2
Suryansh Pandey <suryanshpsurya@gmail.com>
2021-06-13 17:01:54 +0530

    upload

--- granth/ved/rig-sukt.js
+++ granth/ved/rig-sukt.js
@@ -1,93 +1,123 @@
-function change_sukt(val) {
-  select_mandal = val.value;
-  // var $selectsukt = $("#rig-sukt");
-  switch (select_mandal) {
-    case "1":
-      sukt(191);
-      break;
-    case "2":
-      sukt(43);
-      break;
-    case "3":
-      sukt(62);
-      break;
-    case "4":
-      sukt(58);
-      break;
-    case "5":
-      sukt(87);
-      break;
-    case "6":
-      sukt(75);
-      break;
-    case "7":
-      sukt(104);
-      break;
-    case "8":
-      sukt(103);
-      break;
-    case "9":
-      sukt(114);
-      break;
-    case "10":
-      sukt(191);
-      break;
-  }
+"use strict"
 
+async function change_sukt(val) {
+  var select_mandal = val.value;
+  let url = "pages/rigsukt_sankhya.json?"+select_mandal;
+  const settings = {
+    method: "GET", // POST, PUT, DELETE, etc.
+    headers: {
+      // the content type header value is usually auto-set
+      // depending on the request body
+      "Content-Type": "application/json"
+    },
+  };
+
+  const response = await fetch(url, settings);
+  let arrData = await response.json();
+  for (let z in arrData) {
+    var mandal = arrData[z].mandal;
+    var sukts = arrData[z].sukts;
+    if (mandal === select_mandal) {
+      sukt(sukts);
+    }
+  }
 }
 
 function sukt(num) {
+  var select_mandal = document.getElementById("rig-mandal").value;
   var selectsukt = document.getElementById("rig-sukt");
   selectsukt.innerHTML = "<option value=\"\" disabled selected>सूक्त चुनें</option>";
-  for (i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     var option = document.createElement("option");
-    option.value = select_mandal+"/"+i;
+    option.value = select_mandal + "/" + i;
     option.text = i;
     selectsukt.appendChild(option);
   }
 }
 
-function change_mantra(val) {
-  select_sukt = val.value;
-  // var $selectsukt = $("#rig-sukt");
-  switch (select_sukt) {
-    case "1/1":
-      mantra(9);
-      break;
-    case "1/2":
-      mantra(20);
+
+async function change_mantra(val) {
+  var select_sukt = val.value;
+  let url = "pages/rigmantra_sankhya.json?"+select_sukt;
+  const settings = {
+    method: "GET", // POST, PUT, DELETE, etc.
+    headers: {
+      // the content type header value is usually auto-set
+      // depending on the request body
+      "Content-Type": "application/json"
+    },
+  };
+
+  const response = await fetch(url, settings);
+  console.log(response);
+  let arrData = await response.json();
+  console.log(arrData);
+  for (let z in arrData) {
+    var sukts = arrData[z].sukts;
+    console.log(sukts);
+    var mantras = arrData[z].mantra;
+    if (sukts === select_sukt) {
+      mantra(mantras);
+    }
   }
 }
 
 function mantra(num) {
-  selectmantra = document.getElementById("rig-mantra");
+  var select_sukt = document.getElementById("rig-sukt").value;
+  var selectmantra = document.getElementById("rig-mantra");
   selectmantra.innerHTML = "<option value=\"\" disabled selected>मन्त्र चुनें</option>";
-  for (j = 1; j <= num; j++) {
+  for (let j = 1; j <= num; j++) {
     var option = document.createElement("option");
-    option.value = select_sukt+"/"+j;
+    option.value = select_sukt + "/" + j;
     option.text = j;
     selectmantra.appendChild(option);
   }
 }
 
-function show_mantra(val) {
-  x = val.value;
-  load(x);
-}
-function load(mantra) {
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var parse = JSON.parse(this.responseText);
-      for (x in parse) {
-        var parsemantra = parse[x].id;
-        if (parsemantra === mantra) {
-          var smantra = parse[x].mantra;
-          document.getElementById("demo").innerHTML = parsemantra+"+"+smantra;
-        }
-      }
+// function show_mantra(val) {
+//   var x = val.value;
+//   load(x);
+// }
+
+async function show_mantra(val) {
+  var mantra = val.value;
+  let url = "pages/rigData.json?"+mantra;
+ 
+ 
+  const response = await fetch(url);
+  const arrData = await response.json();
+  for (var x in arrData) {
+    var parsemantra = arrData[x].id;
+    if (parsemantra === mantra) {
+      // var id = arrData[x].id;
+      var mandal = arrData[x].mandal;
+      var sukt = arrData[x].sukt;
+      var mantra = arrData[x].mantra;
+      var devta = arrData[x].devta;
+      var rishi = arrData[x].rishi;
+      var chhand = arrData[x].chhand;
+      var swar = arrData[x].swar;
+      var mantra_hi = arrData[x].mantra_hi;
+      var padpath = arrData[x].padpath;
+
+      var rig_box = document.getElementById("rig_box");
+      rig_box.classList.add("show_mantra");
+      $('body').css({
+        overflow: 'hidden'
+      });
+
+      // document.getElementById("id").innerHTML = id;
+      document.getElementById("mandal").innerHTML = mandal;
+      document.getElementById("sukt").innerHTML = sukt;
+      document.getElementById("mantra").innerHTML = mantra;
+      document.getElementById("devta").innerHTML = devta;
+      document.getElementById("rishi").innerHTML = rishi;
+      document.getElementById("chhand").innerHTML = chhand;
+      document.getElementById("swar").innerHTML = swar;
+      document.getElementById("mantra_hi").innerHTML =mantra_hi;
+      document.getElementById("padpath").innerHTML = padpath;
+
     }
-  };
-  http.open("GET", "pages/rigData.json", true);
-  http.send();
+  }
 }
+
